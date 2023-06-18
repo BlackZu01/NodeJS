07358bb6ef96e5c663aec1a8b1d64b335cada985
BlackZu01 <citozuluaga44@gmail.com>
2023-06-18 15:16:01 -0500

    First steps in NodeJS

---
+++ app.js
@@ -0,0 +1,4 @@
+const { helloWorld, saludar } = require('./saludos');
+
+console.log(saludar('María'));
+console.log(helloWorld());
---
+++ saludos.js
@@ -0,0 +1,12 @@
+saludar = (name) => {
+    return `Hello ${name}`;
+}
+
+helloWorld = () => {
+    return 'Hello, World';
+}
+
+module.exports = {
+    saludar: saludar,
+    helloWorld: helloWorld
+}

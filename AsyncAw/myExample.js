bcf6456f5c1540115d5efc6853efe553a8228aa8
BlackZu01 <citozuluaga44@gmail.com>
2023-06-25 23:41:38 -0500

    Example finalized

--- AsyncAw/myExample.js
+++ AsyncAw/myExample.js
@@ -4,11 +4,11 @@ const listener = new EventEmitter();
 const types = ['debit', 'credit', 'bank account'];
 
 const economy = (amount) => {
-    console.log(`Your txn of ${amount} will arrive in 3-5 days!`)
+    console.log(`Your txn of ${amount} will arrive in 3-5 days!`);
 }
 
 const express = (amount) => {
-    console.log(`Your txn of ${amount} will arrive inmediately!`)
+    console.log(`Your txn of ${amount} will arrive inmediately!`);
 }
 
 listener.on('debit', express);
@@ -16,4 +16,37 @@ listener.on('credit', express);
 
 listener.on('bank account', economy);
 
+const initTxn = (type, amount) => {
+    console.log('Thanks a lot for trusting in Remitly!');
+    return new Promise((resolve, reject) => {
+        console.log('Initilizing transaction process...');
+        setTimeout(() => {
+            if (types.includes(type)) {
+                resolve(listener.emit(type, amount));
+            } else {
+                reject('Invalid payment method !!');
+            }
+        }, 2000);
+    });
+}
 
+const endingTxn = () => {
+    return new Promise((resolve, reject) => {
+        console.log('Tracking transaction...');
+        setTimeout(() => {
+            resolve('Transaction approved!');
+        }, 3000);
+    });
+}
+
+initTxn('debit', 773)
+    .then((response) => {
+        response;
+        return endingTxn();
+    })
+    .then((responseProcess)=> {
+        console.log(responseProcess);
+    })
+    .catch(error => {
+        console.log(error);
+    })
